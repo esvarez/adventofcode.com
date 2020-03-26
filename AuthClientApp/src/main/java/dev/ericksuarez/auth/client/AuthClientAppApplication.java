215668c41ce2bbe5e42c936a8fdab369f74a2a64
Erick Suarez <ericksuarezdev@gmail.com>
2020-03-26 17:44:07 -0600

    Tried 1

---
+++ AuthClientApp/src/main/java/dev/ericksuarez/auth/client/AuthClientAppApplication.java
@@ -0,0 +1,13 @@
+package dev.ericksuarez.auth.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthClientAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthClientAppApplication.class, args);
+	}
+
+}
---
+++ AuthClientApp/src/test/java/dev/ericksuarez/auth/client/AuthClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ericksuarez.auth.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthClientAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
