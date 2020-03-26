c291770afef7d2a7f0982dfff76007029cdc8fae
Erick Suarez <ericksuarezdev@gmail.com>
2020-03-25 23:01:17 -0600

    Work on progress...

--- AuthServerApp/src/main/java/dev/ericksuarez/auth/server/AuthServerAppApplication.java
+++ AuthServerApp/src/main/java/dev/ericksuarez/auth/server/AuthServerAppApplication.java
@@ -2,8 +2,10 @@ package dev.ericksuarez.auth.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class AuthServerAppApplication {
 
 	public static void main(String[] args) {
