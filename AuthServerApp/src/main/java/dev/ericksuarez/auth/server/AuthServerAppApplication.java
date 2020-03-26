6d1c48cb5f9655f7dfd084ea27dc68b178e0d3e8
Erick Suares <ericksuarezdev@gmail.com>
2020-03-25 21:44:46 -0600

    Add Oauth Server

---
+++ AuthServerApp/src/main/java/dev/ericksuarez/auth/server/AuthServerAppApplication.java
@@ -0,0 +1,13 @@
+package dev.ericksuarez.auth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerAppApplication.class, args);
+	}
+
+}
---
+++ AuthServerApp/src/test/java/dev/ericksuarez/auth/server/AuthServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ericksuarez.auth.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
