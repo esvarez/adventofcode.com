794f058ff07f2d36e23d38681c3c20d57ee04d65
Erick Suarez <ericksuarezdev@gmail.com>
2020-03-27 23:56:00 -0600

    Dependencies dont work

---
+++ AuthClientApp/src/main/java/dev/ericksuarez/auth/client/WebSecurityConfiguration.java
@@ -0,0 +1,8 @@
+package dev.ericksuarez.auth.client;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration {
+
+}
--- AuthServerApp/src/main/java/dev/ericksuarez/auth/server/AuthServerAppApplication.java
+++ AuthServerApp/src/main/java/dev/ericksuarez/auth/server/AuthServerAppApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class AuthServerAppApplication {
 
 	public static void main(String[] args) {
